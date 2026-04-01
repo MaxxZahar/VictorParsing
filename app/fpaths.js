ccca36346783b01618d1cc1fd97287269746d5b8
MaxxZahar <maxxzahar@rambler.ru>
2026-04-01 15:11:35 +0300

    l update baseball

--- app/fpaths.js
+++ app/fpaths.js
@@ -2087,7 +2087,54 @@ const paths = {
         'table': 'https://www.flashscore.co.uk/volleyball/ukraine/superleague/standings/#/23OXsI1S/standings/overall/',
         'fixtures': 'https://www.flashscore.co.uk/volleyball/ukraine/superleague/fixtures/',
     },
-
+    'Serie Nacional': {
+        'name': 'Serie Nacional',
+        'sport': 'Baseball',
+        'country': 'Cuba',
+        'numberOfTeams': 16,
+        'table': 'https://www.flashscore.co.uk/baseball/cuba/serie-nacional/standings/#/hbIpxsAn/standings/overall/',
+        'fixtures': 'https://www.flashscore.co.uk/baseball/cuba/serie-nacional/fixtures/',
+    },
+    'Extraliga CZEB': {
+        'name': 'Extraliga',
+        'sport': 'Baseball',
+        'country': 'Czech Republic',
+        'numberOfTeams': 8,
+        'table': 'https://www.flashscore.co.uk/baseball/czech-republic/extraliga/standings/#/GlXOIbJa/standings/overall/',
+        'fixtures': 'https://www.flashscore.co.uk/baseball/czech-republic/extraliga/fixtures/',
+    },
+    'Division 1': {
+        'name': 'Division 1',
+        'sport': 'Baseball',
+        'country': 'France',
+        'numberOfTeams': 8,
+        'table': 'https://www.flashscore.co.uk/baseball/france/division-1/standings/#/ngFONs6K/standings/overall/',
+        'fixtures': 'https://www.flashscore.co.uk/baseball/france/division-1/fixtures/',
+    },
+    'Hoofdklasse': {
+        'name': 'Hoofdklasse',
+        'sport': 'Baseball',
+        'country': 'Netherlands',
+        'numberOfTeams': 8,
+        'table': 'https://www.flashscore.co.uk/baseball/netherlands/hoofdklasse/standings/#/2RxD2AE2/standings/overall/',
+        'fixtures': 'https://www.flashscore.co.uk/baseball/netherlands/hoofdklasse/fixtures/',
+    },
+    'KBO': {
+        'name': 'KBO',
+        'sport': 'Baseball',
+        'country': 'South Korea',
+        'numberOfTeams': 10,
+        'table': 'https://www.flashscore.co.uk/baseball/south-korea/kbo/standings/#/rojGWW5k/standings/overall/',
+        'fixtures': 'https://www.flashscore.co.uk/baseball/south-korea/kbo/fixtures/',
+    },
+    'LVBP': {
+        'name': 'LVBP',
+        'sport': 'Baseball',
+        'country': 'Venezuela',
+        'numberOfTeams': 8,
+        'table': 'https://www.flashscore.co.uk/baseball/venezuela/lvbp/standings/#/CM7RpuCL/standings/overall/',
+        'fixtures': 'https://www.flashscore.co.uk/baseball/venezuela/lvbp/fixtures/',
+    },
 }
 
 module.exports = paths;
